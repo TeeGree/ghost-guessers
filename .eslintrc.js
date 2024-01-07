64f591a73ca16e365aba2f62d00d5252aa6df9f6
TeeGree <tsgriffi@gmail.com>
2024-01-07 18:59:17 -0500

    Upgrade eslint and set up very simple express server.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,19 @@ module.exports = {
     // add more generic rulesets here, such as:
     // 'eslint:recommended',
     "plugin:vue/vue3-recommended",
+    "plugin:prettier/recommended",
     // 'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
   ],
   rules: {
     // override/add rules settings here, such as:
     // 'vue/no-unused-vars': 'error'
+    "vue/html-self-closing": "off",
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
   },
+  plugins: ["prettier"],
 };
